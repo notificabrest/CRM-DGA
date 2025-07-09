e4f503b535354edd5ec128ce961f531271974fbb
notificabrest <notificabrest@gmail.com>
2025-07-08 21:18:07 -0300

    Updated AnalyticsPage.tsx

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -248,7 +248,7 @@ const AnalyticsPage: React.FC = () => {
               })}
           </div>
         </div>
-      )}
+      </div>
 
       {/* Performance Metrics */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
